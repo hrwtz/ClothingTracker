6ba2c34982b0e9fff8dc40f7324e3de295c6b447
Mike Hurwitz <mikehurwitz@Mikes-MacBook-Pro.local>
2017-02-12 19:34:42 -0800

    Working on orm and getting data

--- www/app/app.module.js
+++ www/app/app.module.js
@@ -3,84 +3,12 @@
 
 	angular
 		.module('clothingTracker', ['ionic', 'ngData'])
-		.run(ionicRun)
-		.run(ngDataRun);
+		.run(runBlock);
 	
-	ionicRun.$inject = ['$ionicPlatform'];
+	runBlock.$inject = ['ionicFactory', 'ngDataFactory'];
 
-	function ionicRun ($ionicPlatform) {
-		$ionicPlatform.ready(function() {
-			if(window.cordova && window.cordova.plugins.Keyboard) {
-				// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-				// for form inputs)
-				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-				// Don't remove this line unless you know what you are doing. It stops the viewport
-				// from snapping when text inputs are focused. Ionic handles this internally for
-				// a much nicer keyboard experience.
-				cordova.plugins.Keyboard.disableScroll(true);
-			}
-			if(window.StatusBar) {
-				StatusBar.styleDefault();
-			}
-		});
-	}
-
-	ngDataRun.$inject = ['$ngData'];
-
-	function ngDataRun ($ngData) {
-        $ngData.model('Clothing Item', {
-            tableName: 'clothing_items',
-            properties: {
-            	brand: {
-            		type: String,
-            		required: true
-            	},
-				note: {
-            		type: String,
-            		required: true
-            	},
-            	color: {
-            		type: String,
-            		required: true
-            	},
-				cost: Number,
-				date_created: {
-            		type: Date,
-            		required: true
-            	},
-				category_id: {
-					type: Number,
-					required: true
-				}
-            }
-        });
-
-        $ngData.model('Category', {
-            tableName: 'categories',
-            properties: {
-            	title: {
-            		type: String,
-            		required: true,
-            		unique: true
-            	},
-            	singular_title: {
-            		type: String,
-            		required: true,
-            		unique: true
-            	},
-				date_created: {
-            		type: Date,
-            		required: true
-            	}
-            }
-        });
-
-   //      wear_log
-			// id
-			// clothing_id
-			// date_created
-
-        $ngData.initialize();
+	function runBlock (ionicFactory, ngDataFactory) {
+		ionicFactory.initialize();
+		ngDataFactory.initialize();
 	}
 })();
--- www/app/components/home/home.controller.js
+++ www/app/components/home/home.controller.js
@@ -16,22 +16,96 @@
 		Category = $ngData.model('Category');
 		ClothingItem = $ngData.model('Clothing Item');
 
-		activate();
+		init();
 
 		return;
 
-		function activate () {
-			var promises = [Category.find(), ClothingItem.find()];
+		function init () {
+			var contain = {
+				Category: {
+					'Clothing Item': true
+				}
+			};
 
-			$q.all(promises).then(function (values) {
-				vm.categories = values[0];
-            	vm.clothingItems = values[1];
-
-            	nestData();
+			find('Category', {}, {'Clothing Item': true}).then(function (values) {
+				vm.categories = values['Category'];
+            	vm.clothingItems = values['Clothing Item'];
+            	nestData(contain, values);
 			});
 		}
 
-		function nestData () {
+		function find (modelName, conditions, contain, finalResults) {
+			var d = $q.defer();
+			var model = $ngData.model(modelName);
+
+			conditions = conditions || [];
+			contain = contain || {};
+			finalResults = finalResults || {};
+
+			var query = getQuery(model, conditions);
+
+			query.then(function (results) {
+				finalResults[modelName] = results;
+				var resultIds = _.pluck(results, 'id');
+				if (Object.keys(contain).length) {
+					var allPromises = [];
+					for (var containModel in contain) {
+						allPromises.push(find(containModel, {[$ngData.model(modelName).definition.referenceName + '_id']: {$in: resultIds}}, contain[containModel], finalResults));
+					}
+					$q.all(allPromises).then(function () {
+						d.resolve(finalResults);
+					});
+				} else {
+					d.resolve(finalResults);
+				}
+			});
+			return d.promise;
+		}
+
+		function getQuery (model, conditions) {
+			var query;
+			if (!Object.keys(conditions).length) {
+				query = model.find();
+			} else {
+				for (var i = 0; i < Object.keys(conditions).length; i++) {
+					var method = i === 0 ? 'where' : 'and';
+					query = model[method](Object.keys(conditions)[i], conditions[Object.keys(conditions)[i]]);
+				}
+			}
+			return query;
+		}
+
+		function nestData (contain, values, result) {
+			if (!result) {
+				result = values[Object.keys(contain)[0]];
+			}
+			contain = contain[Object.keys(contain)[0]];
+
+			result.forEach(function (object) {
+				var contains = Object.keys(contain);
+				contains.forEach(function (containModel) {
+					object[$ngData.model(containModel).tableName] = _.where(values[containModel], {category_id: object.id})
+				});
+			});
+
+			vm.categories = result;
+
+			console.log(result);
+			return;
+
+
+			result = result || {};
+
+
+			var contains = Object.keys(contain);
+			contains.forEach(function (containModel) {
+				// result[containModel]
+			});
+
+
+			console.log(contain);
+			console.log(values);
+			return;
 			// Set up hash so we can efficiently know the index and set up empty array
 			// to store clothing items in
 			var hash = {};
@@ -45,7 +119,6 @@
 			vm.clothingItems.forEach(function (clothingItem) {
 				vm.categories[hash[clothingItem.category_id]].clothingItems.push(clothingItem);
 			});
-			console.log(vm.categories);
 		}
 
 
